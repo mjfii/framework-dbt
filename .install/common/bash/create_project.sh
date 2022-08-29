b30e0bfb9096a96c29aa5c1f605407e9bd1df690
Michael Flanigan <7891356+mjfii@users.noreply.github.com>
2022-08-29 10:09:20 -0600

    [UPDATE] updated project creation scripts

--- .install/common/bash/create_project.sh
+++ .install/common/bash/create_project.sh
@@ -1,4 +1,4 @@
 function create_project() {
   cd "$1" || exit
-  python3 ./.install/common/py/create_project.py "$1" > dbt_project.yml
+  python3 ./.install/common/py/create_project.py "$1" > ./.install/install.log
 }
--- .install/common/py/create_project.py
+++ .install/common/py/create_project.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import sys
+import os
 import yaml
 from jinja2 import Template
 
 _pwd = sys.argv[1]
 _config_file = f"{_pwd}/.install/config/setup.yml"
 _template_file = f"{_pwd}/.install/templates/dbt_project.yml"
+_project_file = f"{_pwd}/dbt_project.yml"
 
 with open(_template_file, "r") as _config:
     _template = Template(_config.read())
@@ -19,4 +21,6 @@ with open(_config_file, "r") as _config:
     except yaml.YAMLError as _excep:
         print(_excep)
 
-print(_template.render(profile=f"{_profile}-{_adapter}", project=_project))
+if os.stat(_project_file).st_size == 0:
+    with open(_project_file, "w") as _config:
+        _config.write(_template.render(profile=f"{_profile}-{_adapter}", project=_project))
